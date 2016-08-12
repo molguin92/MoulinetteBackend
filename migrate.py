a11702ab496f0ab8b181267c749c53fafe120ffe
Manuel Olguin <molguin@dcc.uchile.cl>
2016-08-12 15:34:44 -0400

    added migrate.py

---
+++ migrate.py
@@ -0,0 +1,7 @@
+from moulinette import app, db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
