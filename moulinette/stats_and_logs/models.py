9fa450f33aae1ad57380cf2c0f0c9b9d9672533c
Manuel Olguin <molguin@dcc.uchile.cl>
2016-08-27 15:48:48 -0300

    RequestLog

---
+++ moulinette/stats_and_logs/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from moulinette import db
+
+
+class RequestLog(db.Model):
+    __tablename__ = 'requestlog'
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.now)
+    updated = db.Column(db.DateTime, default=datetime.now,
+                        onupdate=datetime.now)
+
+    test_id = db.Column(db.Integer, db.ForeignKey('test.id'), nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'),
+                          nullable=False)
+    result = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, test_id, client_id, result):
+        self.test_id = test_id
+        self.client_id = client_id
+        self.result = result
