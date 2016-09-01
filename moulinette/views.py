284dc22b13016949f0b9c2e4f1d2fc0b2417c687
Manuel Olguin <molguin@dcc.uchile.cl>
2016-09-01 11:48:07 -0300

    added dedicated endpoint for file downloads

--- moulinette/views.py
+++ moulinette/views.py
@@ -3,6 +3,12 @@ from moulinette.client.views import *
 from moulinette.homework.views import *
 from moulinette.stats_and_logs.views import *
 
+
+class StaticFilesResource(Resource):
+    def get(self, filename):
+        return app.send_static_file('public/{fname}'.format(fname=filename))
+
+
 # This file declares all the endpoints of the API.
 
 api.add_resource(HomeworkCollectionResource, '/api/v1/homeworks',
@@ -11,3 +17,4 @@ api.add_resource(HomeworkResource, '/api/v1/homeworks/<string:hwid>')
 api.add_resource(TestResource, '/api/v1/validate_tests')
 api.add_resource(ClientResource, '/api/v1/clients')
 api.add_resource(LogResource, '/api/v1/logs')
+api.add_resource(StaticFilesResource, '/files/<string:filename>')
