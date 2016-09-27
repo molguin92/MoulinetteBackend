6c759e7a7db5d12b68db780774d97c6b023977ec
Manuel Olguin <molguin@dcc.uchile.cl>
2016-09-26 21:48:18 -0300

    test deleter

--- delete_test.py
+++ delete_test.py
@@ -1,6 +1,8 @@
+from moulinette import testserializer
 from moulinette.homework.models import *
 
 id = input()
-test = Test.query.get(id)
+
+test = Test.query.get(testserializer.loads(id))
 db.session.delete(test)
 db.session.commit()
