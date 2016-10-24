1eb80fa428a2b602710919b8353d98d3b684dccd
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-24 00:10:00 -0300

    fix t3 output

---
+++ max_subarray.py
@@ -0,0 +1,54 @@
+def max_subarray(A):
+    max_ending_here = max_so_far = A[0]
+    start = 0
+    end = 0
+    ts = 0
+    te = 0
+    for i in range(1, len(A)):
+        x = A[i]
+        sum = max_ending_here + x
+        if sum > x:
+            te = i
+            max_ending_here = sum
+        else:
+            ts = te = i
+            max_ending_here = x
+
+        if max_so_far < max_ending_here:
+            start = ts
+            end = te
+            max_so_far = max_ending_here
+
+        elif max_so_far == max_ending_here and end - start > te - ts:
+            start = ts
+            end = te
+
+    return start, end
+
+
+def get_output(str_in):
+    str_in = str_in.split('\n')
+    str_out = ''
+    for line in str_in:
+        line = line.split(' ')
+        A = []
+        for item in line:
+            A.append(int(item))
+
+        start, end = max_subarray(A)
+        str_out += str(start) + ',' + str(end) + '\n'
+
+    return str_out
+
+
+if __name__ == '__main__':
+    assert max_subarray([-200, 50, 10, -20, 30, 40, -50, -40, 50, -200]) == \
+           (1, 5)
+    assert max_subarray([-200, 50, 10, -20, 30, 40, -50, -40, 50, -200, 50,
+                         10, -20, 30, 40, -50, -40, 50, -200]) == (1, 5)
+
+    assert max_subarray([1, 1, 1, -10, 3]) == (4, 4)
+    assert max_subarray([0, 0, 0, 0, 0]) == (0, 0)
+    assert max_subarray([4, 0, 0, 0, 0]) == (0, 0)
+    assert max_subarray([-123, -2, -3, -6, -10]) == (1, 1)
+    assert max_subarray([1, 3, 4, 7, 9]) == (0, 4)
--- remove_commas.py
+++ remove_commas.py
@@ -1,5 +1,6 @@
 import click
 
+from max_subarray import get_output
 from moulinette import hwserializer
 from moulinette.homework.models import *
 
@@ -12,7 +13,7 @@ if __name__ == '__main__':
         tests = Test.query.filter(Test.item_id == item.id).all()
         for test in tests:
             test.stdin = test.stdin.replace(',', '')
-            test.stdout = test.stdout.replace(' ', ',')
+            test.stdout = get_output(test.stdin)
             db.session.add(test)
 
         db.session.add(item)
