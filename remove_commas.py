38eef6b911c3352272ec306aa419f2d17c34b354
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-24 10:19:47 -0300

    fix negatives

--- remove_commas.py
+++ remove_commas.py
@@ -1,8 +1,26 @@
+from random import random
+
 import click
 
 from max_subarray import get_output
 from moulinette import hwserializer
 from moulinette.homework.models import *
+
+
+def generate_array(length, min_val, max_val):
+    result = []
+    for i in range(length):
+        result.append(random.randint(min_val, max_val))
+    return result
+
+
+def is_negative(l):
+    for item in l:
+        if int(item) >= 0:
+            return False
+
+    return True
+
 
 if __name__ == '__main__':
     hwid = click.prompt('HW to fix:', type=str)
@@ -12,9 +30,20 @@ if __name__ == '__main__':
     for item in items:
         tests = Test.query.filter(Test.item_id == item.id).all()
         for test in tests:
-            test.stdin = test.stdin.replace(',', '')
+            lstdout = test.stdout.strip().split('\n')
+            for i in range(len(lstdout)):
+                line = lstdout(i)
+                linel = line.split(' ')
+                while is_negative(linel):
+                    A = generate_array(len(linel), random.randint(-500, 0),
+                                       random.randint(0, 500))
+                    line = str(A)[1:len(A) - 1].replace(',', '')
+                    linel = line.split(' ')
+                lstdout[i] = line
+
+            test.stdin = '\n'.join(lstdout)
             test.stdout = get_output(test.stdin)
-            test.timeout = 180
+            test.timeout = 600
             db.session.add(test)
 
         db.session.add(item)
