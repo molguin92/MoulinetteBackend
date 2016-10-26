7620e5b2d7065108b2063c35b743bb7d1d78a496
Manuel Olguin <molguin@dcc.uchile.cl>
2016-10-25 22:46:00 -0300

    count submissions per homework

---
+++ submissions_per_homework.py
@@ -0,0 +1,26 @@
+from moulinette import hwserializer
+from moulinette.homework.models import *
+from moulinette.stats_and_logs.models import *
+
+
+def get_submissions_hw(id):
+    count = 0
+    items = Item.query.filter(Item.homework_id == id).all()
+    for item in items:
+        tests = Test.query.filter(Test.item_id == item.id).all()
+        for test in tests:
+            submissions = RequestLog.query.filter(RequestLog.test_id ==
+                                                  test.id).all()
+            count += len(submissions)
+
+    return count
+
+
+if __name__ == '__main__':
+    homeworks = Homework.query.all()
+    for homework in homeworks:
+        serialid = hwserializer.dumps(homework.id)
+        print('ID: ' + serialid)
+        print('Name: ' + homework.name)
+        print('Count: ' + get_submissions_hw(homework.id))
+        print()
